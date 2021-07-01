f0c48db479cffba6bbdb0943efa202c816da0264
sinqx <41581873+sinqx@users.noreply.github.com>
2021-07-01 23:50:22 +0600

    pls accept

--- final/src/main/java/com/example/demo/repository/LogsRepository.java
+++ final/src/main/java/com/example/demo/repository/LogsRepository.java
@@ -10,6 +10,6 @@ import java.util.List;
 
 @Repository
 public interface LogsRepository extends JpaRepository<Logs, Long> {
-    List<Logs> findByRecordedIsBetweenAndCitizenVaccinatedTrueOrderByCitizenRegionName(LocalDateTime startOfDay, LocalDateTime  endOfDay);
+    List<Logs> findByRecordedIsBetweenAndCitizenVaccineIsNotNullOrderByCitizenRegionName(LocalDateTime startOfDay, LocalDateTime  endOfDay);
     List<Logs> findByRecordedIsBetweenAndCitizenInfectedTrueOrderByCitizenRegionName(LocalDateTime startOfDay, LocalDateTime  endOfDay);
 }
